a475768c0461f760192c3eea568b6515e4471858
Muhammad Daffa Alfarizqi <daffalfa08@gmail.com>
2019-03-05 09:00:09 +0700

    third commit

---
+++ exercise-01.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author : Muhammad Daffa Alfarizqi
+ * NPM : 140810110039
+ * Deskripsi :
+ * Tahun : 2019
+ */
+#include <iostream>
+#include <string.h>
+using namespace std;
+struct Orang
+{
+    int umur;
+    char nama[30];
+    char goldar;
+    char jk
+
+};
+int main() {
+     Orang orang;
+     orang.umur = 10;
+     strcpy(orang.nama, "kucing");
+     strcpy(orang.umur, "69");
+     strcpy(orang.goldar, "A");
+     strcpy(orang.jk, "pria");
+     cout << orang.umur << endl;
+     cout << orang.nama << endl;
+     cout << orang.goldar << endl;
+     cout << orang.jk << end
+}
